be55dd65ae277d9286501eeb87de4d73588fbe8d
alimukhammad <47307714+alimukhammad@users.noreply.github.com>
2021-01-25 06:33:11 -0500

    num.1

--- Chapter_4/magicians.py
+++ Chapter_4/magicians.py
@@ -1,3 +1,5 @@
 magicians =['sam', 'david', 'charly']
 for i in magicians:
-    print(i)
+    print(i.title() + ", that was great trick!")
+    print("I can't wait to see your next trick, " + i.title() + ".\n")
+print("Thank you, everyone. That was great")
