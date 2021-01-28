824269466e0e165e86c5f93e9cb680458304b264
alimukhammad <47307714+alimukhammad@users.noreply.github.com>
2021-01-27 20:55:45 -0500

    Add files via upload

---
+++ Chapter_5/amusement_park.py
@@ -0,0 +1,9 @@
+
+age = int(input("Enter age: "))
+
+if age <4:
+    print("Your admission cost is $0")
+elif age <18:
+    print("Your admission cost is $5")
+else:
+    print("Your admission cost is $10")
---
+++ Chapter_5/toppings.py
@@ -0,0 +1,15 @@
+x = ['mushrooms','extra cheese']
+
+if x:
+    for i in x:
+        print("Adding " + i)
+else:
+    print("are you sure you want a plain pizza?")
+# if 'anchovies' in x:
+#     print("Hold the anchovies")
+# elif 'pepperoni' in x:
+#     print("Hold the pepperoni")
+# elif 'extra cheese' in x:
+#     print("Hold the extra cheese")
+
+print("\nFinished making your pizza")
